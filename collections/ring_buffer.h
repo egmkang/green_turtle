8fef55700da33a7f88b52b89341aa5654e9b58b3
egmkang wang <egmkang@gmail.com>
2012-04-19 23:55:27 +0800

    ring buffer added

---
+++ collections/ring_buffer.h
@@ -0,0 +1,168 @@
+//Copyright 2012, egmkang wang.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// auhor: egmkang (egmkang@gmail.com)
+
+#ifndef __RING_BUFFER__
+#define __RING_BUFFER__
+#include <assert.h>
+#include <cstring>
+#include <algorithm>
+#include <cstddef>
+#include <vector>
+
+namespace green_turtle{namespace collections{
+
+template<class T>
+class RingBuffer{
+ public:
+  RingBuffer(size_t buffer_size_);
+  ~RingBuffer();
+
+  size_t  Read(T* dest,size_t count);
+  size_t  Write(const T* src,size_t count);
+  void    Reset();
+  void    MoveReadPos(int offset);
+  void    MoveWritePos(int offset);
+  T*      GetBegin() const;
+  T*      GetEnd() const;
+  size_t  GetSize() const;
+  size_t  GetTailSpace() const;
+ private:
+  size_t          write_;
+  size_t          read_;
+  size_t          index_mark_;
+  std::vector<T>  array_;
+};
+template<class T>
+RingBuffer<T>::RingBuffer(size_t buffer_size_):
+  write_(0)
+  ,read_(0)
+  ,index_mark_(0)
+{
+  assert(!(buffer_size_ & (buffer_size_-1)));
+  index_mark_ = buffer_size_ - 1;
+  array_.resize(buffer_size_);
+}
+template<class T>
+RingBuffer<T>::~RingBuffer()
+{
+}
+template<class T>
+size_t  RingBuffer<T>::Read(T* dest,size_t count)
+{
+  assert(dest && count);
+  assert(read_ <= write_);
+  size_t size_      = GetSize();
+  if(count > size_)
+    count = size_;
+
+  size_t from = read_ & index_mark_;
+  size_t to   = (read_ + count) & index_mark_;
+  if(from <= to)
+  {
+    std::memcpy(dest, &array_[from], count * sizeof(T));
+  }
+  else
+  {
+    size_t first_read_ = index_mark_ + 1 - from;
+    std::memcpy(dest, &array_[from], first_read_ * sizeof(T));
+    std::memcpy(dest+first_read_, &array_[0], to * sizeof(T));
+  }
+  read_ += count;
+  return count;
+}
+template<class T>
+size_t  RingBuffer<T>::Write(const T* src,size_t count)
+{
+  assert(src && count);
+  assert(read_ <= write_);
+  size_t space_ = index_mark_ + 1 - GetSize();
+  if(count > space_)
+    count = space_;
+  size_t from = write_ & index_mark_;
+  size_t to   = (write_ + count) & index_mark_;
+  if(from <= to)
+  {
+    std::memcpy(&array_[from],src,count * sizeof(T));
+  }
+  else
+  {
+    size_t first_write_ = index_mark_ + 1 - from;
+    std::memcpy(&array_[from], src, first_write_ * sizeof(T));
+    std::memcpy(&array_[0], src + first_write_, to * sizeof(T));
+  }
+  write_ += count;
+  return count;
+}
+template<class T>
+void    RingBuffer<T>::Reset()
+{
+  size_t size_ = GetSize();
+  std::memmove(&array_[0], GetBegin(), size_ * sizeof(T));
+  write_ -= read_;
+  read_ = 0;
+}
+template<class T>
+void    RingBuffer<T>::MoveReadPos(int offset)
+{
+  assert( read_ + offset > 0 && read_ + offset <= write_);
+  read_ += offset;
+}
+template<class T>
+void    RingBuffer<T>::MoveWritePos(int offset)
+{
+  assert(write_ + offset > 0 && write_ + offset >= read_);
+  write_ += offset;
+}
+template<class T>
+T*      RingBuffer<T>::GetBegin() const
+{
+  return & array_[ read_ & index_mark_ ];
+}
+template<class T>
+T*      RingBuffer<T>::GetEnd() const
+{
+  return & array_[ write_ & index_mark_ ];
+}
+template<class T>
+size_t  RingBuffer<T>::GetSize() const
+{
+  assert(read_ <= write_);
+  return write_ - read_;
+}
+template<class T>
+size_t  RingBuffer<T>::GetTailSpace() const
+{
+  return index_mark_ - (write_ & index_mark_);
+}
+
+};
+};
+#endif
---
+++ test/ring_buffer_test.cc
@@ -0,0 +1,25 @@
+#include "../collections/ring_buffer.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+using namespace green_turtle::collections;
+
+int main()
+{
+  RingBuffer<char> buffer(16);
+  const char *message_in = "0123456789";
+  char message_out[24];
+  int count  = 0;
+  while(count++ < 10)
+  {
+    buffer.Write(message_in,strlen(message_in));
+    memset(message_out,0,sizeof(message_out));
+    buffer.Read(&message_out[0], sizeof(message_out));
+    printf(message_out);
+    printf("\n");
+  }
+
+  return 0;
+}
