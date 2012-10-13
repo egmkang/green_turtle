d16a2b4addae94137d32746b03dd58681c1e9cf4
egmkang wang <egmkang@gmail.com>
2012-10-13 17:20:40 +0800

    restore echo client

--- test/net/echo_client.cc
+++ test/net/echo_client.cc
@@ -14,6 +14,31 @@ using namespace green_turtle::net;
 
 static std::atomic<long> recv_message_count_(0);
 
+class EchoMessage : public Message
+{
+ public:
+  EchoMessage(const char *data)
+      :data_(data)
+  {
+  }
+  EchoMessage(const char *data, size_t len)
+      :data_(data, data+len)
+  {
+  }
+  void* data() const { return (void*)&data_[0]; }
+  size_t length() const { return data_.length(); }
+ private:
+  std::string data_;
+};
+
+static char * NewEchoString(int& send_times_)
+{
+  char * str = (char*)malloc(100);
+  snprintf(str, 100, "this is echo string sent by client, %d times",send_times_);
+  send_times_++;
+  return str;
+}
+
 class BroadCastClient : public TcpClient
 {
  public:
@@ -31,7 +56,11 @@ class BroadCastClient : public TcpClient
     }
     if(size)
     {
-      //this->SendMessage(message);
+      data.SkipRead(size);
+      char *str = NewEchoString(this->send_times_);
+      std::shared_ptr<Message> message(new EchoMessage(str));
+      this->SendMessage(message);
+      free(str);
 
       recv_message_count_.store(count + 1, std::memory_order_release);
     }
@@ -61,7 +90,11 @@ int main()
     assert(!errorCode);
     client->set_events(kEventReadable | kEventWriteable);
 
-    //TODO:egmkang
+    int num = 0;
+    char *str = NewEchoString(num);
+    std::shared_ptr<Message> message(new EchoMessage(str));
+    client->SendMessage(message);
+    free(str);
 
     loop.AddEventHandler(client);
   }
