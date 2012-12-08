972fa6da6ca748205e7ce682f4673ca58557c408
egmkang wang <egmkang@gmail.com>
2012-12-08 15:56:17 +0800

    add ref ptr null test

--- test/ref_obj_test.cc
+++ test/ref_obj_test.cc
@@ -1,5 +1,6 @@
 #include <ref_object.h>
 #include <stdio.h>
+#include <assert.h>
 
 using namespace green_turtle;
 
@@ -22,6 +23,11 @@ int main()
     //Fail
     //const Ptr convert to non const Ptr,
     //RefPtr<ObjA> pConst2 = (const ObjA*)(pObjA);
+
+    RefPtr<ObjA> null;
+    null = pObjA;
+    assert(null);
+    printf("%d\n", null->value);
 
     if(ptr)
     {
