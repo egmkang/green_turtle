9046d3af22681e6538cd44bf8c9071b7ab165cc4
egmkang wang <egmkang@gmail.com>
2012-12-29 16:58:55 +0800

    mysql cxx added

---
+++ green_turtle/mysqlcxx.cc
@@ -0,0 +1,189 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "mysqlcxx.h"
+
+using namespace green_turtle;
+
+SqlCommand& MySqlCommand::SetParam(int index, void *data, int len)
+{
+    assert(index >= 0 && data && len > 0);
+    SetParam(index, MYSQL_TYPE_BLOB, data, len);
+    return *this;
+}
+
+SqlCommand& MySqlCommand::SetParam(int index, const char *data, int len)
+{
+    assert(index >= 0 && data && len > 0);
+    SetParam(index, MYSQL_TYPE_VAR_STRING, data, len);
+    return *this;
+}
+
+void MySqlCommand::SetParam(int index, int type, const void *data, int len)
+{
+    if(this->params.size() <= (size_t)index)
+        this->params.resize(index + 1);
+    SqlParamType &param = params[index];
+    param.param_type = type;
+    param.data.clear();
+    param.data.append((const char*)data, ((const char*)data) + len);
+}
+
+MySqlConnection::MySqlConnection()
+{
+    memset(&conn_, 0, sizeof(conn_));
+    mysql_init(&conn_);
+    mysql_options(&conn_, MYSQL_OPT_RECONNECT, "1");
+}
+
+MySqlConnection::~MySqlConnection()
+{
+    mysql_close(&conn_);
+    memset(&conn_, 0, sizeof(conn_));
+    mysql_init(&conn_);
+    mysql_options(&conn_, MYSQL_OPT_RECONNECT, "1");
+}
+
+int MySqlConnection::Connect(const char* host, unsigned short port, const char* user, const char* passwd, const char* db_name)
+{
+    assert(host && user && passwd && db_name);
+    MYSQL *mysql = mysql_real_connect(&conn_, host, user, passwd, db_name, (unsigned int)(port), NULL, 0);
+    if(!mysql) return -1;
+    mysql_select_db(&conn_, db_name);
+    return 0;
+}
+
+int MySqlConnection::ExecSql(const char *sql)
+{
+    assert(sql);
+    int ret = mysql_real_query(&conn_, sql, (unsigned long)strlen(sql));
+    if(ret)
+    {
+        return -mysql_errno(&conn_);
+    }
+    return mysql_affected_rows(&conn_);;
+}
+
+int MySqlConnection::ExecSql(const SqlCommand &command)
+{
+    MYSQL_STMT *stmt = mysql_stmt_init(&conn_);
+    if(stmt)
+    {
+        const std::string& sql = command.GetSql();
+        mysql_stmt_prepare(stmt, sql.c_str(), sql.size());
+
+        const std::vector<SqlParamType>& params = command.GetParams();
+        std::vector<MYSQL_BIND> binds;
+        binds.resize(params.size());
+        for(unsigned idx = 0; idx < params.size(); ++idx)
+        {
+            memset(&binds[idx], 0, sizeof(binds[idx]));
+            binds[idx].buffer_type  = (enum_field_types)params[idx].param_type;
+            binds[idx].buffer       = params[idx].ptr();
+            binds[idx].buffer_length= params[idx].size();
+        }
+        mysql_stmt_bind_param(stmt, &binds[0]);
+        int ret = mysql_stmt_execute(stmt);
+        if(ret)
+        {
+            mysql_stmt_errno(stmt);
+            return -mysql_errno(&conn_);
+        }
+        int rows = mysql_stmt_affected_rows(stmt);
+        mysql_stmt_close(stmt);
+        return rows;
+    }
+    return -1;
+}
+
+unsigned long long MySqlConnection::GetNewID()
+{
+    return mysql_insert_id(&conn_);
+}
+
+ResultSet* MySqlConnection::ExecSelect(const char *sql)
+{
+    int ret = mysql_real_query(&conn_, sql, (unsigned long)strlen(sql));
+    if(ret) return NULL;
+    MYSQL_RES *res = mysql_store_result(&conn_);
+    if(res)
+        return new MySqlResultSet(res);
+    return NULL;
+}
+
+int MySqlConnection::EscapeString(char *to,const char *from, unsigned long length)
+{
+    assert(to && from && length);
+    return mysql_real_escape_string(&conn_, to, from, length);
+}
+
+MySqlResultSet::MySqlResultSet(MYSQL_RES *result) :
+    result(result, [](MYSQL_RES *res){mysql_free_result(res);}),
+    field_len(NULL),
+    total_fields(0),
+    total_rows(0),
+    current_row(-1)
+{
+    if(result)
+    {
+        this->total_fields = mysql_num_fields(result);
+        this->total_rows = mysql_num_rows(result);
+        if(total_rows)
+        {
+            current_row = 0;
+            row = mysql_fetch_row(result);
+            field_len = mysql_fetch_lengths(result);
+        }
+    }
+}
+
+MySqlResultSet::~MySqlResultSet()
+{
+    //mysql_free_result(result);
+}
+
+int  MySqlResultSet::FieldCount() const
+{
+    return this->total_fields;
+}
+
+int  MySqlResultSet::RowCount() const
+{
+    return this->total_rows;
+}
+
+void MySqlResultSet::Next() const
+{
+    if(this->current_row < this->total_rows)
+    {
+        ++this->current_row;
+        row = mysql_fetch_row(result.get());
+        field_len = mysql_fetch_lengths(result.get());
+    }
+}
+
+bool MySqlResultSet::IsValid() const
+{
+    if(this->total_fields > 0 &&
+            this->field_len &&
+            this->total_rows > 0 &&
+            this->current_row >= 0 &&
+            this->current_row < this->total_rows)
+        return true;
+    return false;
+}
+
+SqlResultVarType MySqlResultSet::operator[] (int field) const
+{
+    if(!IsValid() || field >= this->total_fields)
+        return SqlResultVarType(NULL, 0);
+    const char *data = this->row[field];
+    int len = (unsigned long)this->field_len[field];
+    if(len <= 0) data = NULL;
+    return SqlResultVarType(data, len);
+}
+
+SqlResultVarType MySqlResultSet::at(int field) const
+{
+  return (*this)[field];
+}
---
+++ green_turtle/mysqlcxx.h
@@ -0,0 +1,315 @@
+//Copyright 2012, egmkang wang.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of green_turtle nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// auhor: egmkang (egmkang@gmail.com)
+#ifndef __MYSQL_CXX_H__
+#define __MYSQL_CXX_H__
+#include <memory>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <string.h>
+#include <noncopyable.h>
+
+namespace green_turtle{
+
+class SqlCommand;
+class ResultSet;
+struct SqlResultVarType;
+
+class DBConnection : NonCopyable
+  {
+   public:
+    virtual ~DBConnection(){}
+    /**
+     * connect to database
+     * @param host ip address in string of database
+     * @param port port database server listened
+     * @param user login name
+     * @param passwd password
+     * @param db_name default database name
+     */
+    virtual int Connect(const char* host, unsigned short port, const char* user, const char* passwd, const char* db_name) = 0;
+
+    /**
+     * Execute the sql, and return lines affected(or error number)
+     * @param sql sql that will be executed
+     * @return lines affected. if lines < 0, that means the error number!!!
+     */
+    virtual int ExecSql(const char *sql) = 0;
+    /**
+     * similar with ExecSql(const char*);
+     */
+    virtual int ExecSql(const SqlCommand &command) = 0;
+
+    /**
+     * return the new id that insert sql returned
+     * if previous sql is not insert or return of ExecSql < 0, the behavior is not defined.
+     */
+    virtual unsigned long long GetNewID() = 0;
+
+    /**
+     * Execute one select sql that should return a ResultSet.
+     * @param sql sql will be executed
+     * @return the result set of select
+     */
+    virtual ResultSet* ExecSelect(const char *sql) = 0;
+
+    /**
+     * EscapeString, prevent sql injection
+     * @param to the dest string stored
+     * @param from orign string
+     * @param length from string length
+     * @return error code, 0 is ok
+     */
+    virtual int EscapeString(char *to,const char *from, unsigned long length) = 0;
+  };
+
+/**
+ * The Result Set of executing of a select sql
+ */
+class ResultSet
+{
+ public:
+  virtual ~ResultSet(){}
+
+  /**
+   * get the Field Count of the result set
+   * @return field count
+   */
+  virtual int FieldCount() const = 0;
+
+  /**
+   * get the row count of the result set
+   * return row count
+   */
+  virtual int RowCount() const = 0;
+
+  /**
+   * move the Cursor to the next row
+   */
+  virtual void Next() const = 0;
+
+  /**
+   * check current row is valid
+   * @return true, valid; fale, invalid, should break.
+   */
+  virtual bool IsValid() const = 0;
+
+  /**
+   * get one field in current row, SqlResultVarType can convert to other
+   * type implicitly, or you can get the raw data. @see SqlResultVarType
+   * @param field field index, offset from zero
+   * @return the field value @see SqlResultVarType
+   */
+  virtual SqlResultVarType operator[] (int field) const = 0;
+  virtual SqlResultVarType at(int field) const = 0;
+};
+
+/**
+ * VarType that can implicitly convert to other type, such as
+ * char/short/int/long/long long/char*, etc.
+ * if current value is blob, you can use GetRawData.
+ * YOU CANNOT HOLD any pointer of this class. This class DO NOT manage the
+ * resources.
+ */
+struct SqlResultVarType
+{
+  SqlResultVarType(const char *data, int len) : data(data), len(len){ }
+
+  /**
+   * current value is valid. SQL's NULL is not valid.
+   * @return always return true if value is NOT NULL.
+   */
+  bool IsValid() const { return data && len; }
+
+  /**
+   * implicit convert to int
+   */
+  operator int()                  const { return atoi(data); }
+  /**
+   * implicit convert to unsigned int
+   */
+  operator unsigned int()         const { return atoi(data); }
+  /**
+   * implicit convert to long
+   */
+  operator long()                 const { return atoll(data); }
+  /**
+   * implicit convert to unsigned long
+   */
+  operator unsigned long()        const { return atoll(data); }
+  /**
+   * implicit convert to long long
+   */
+  operator long long()            const { return atoll(data); }
+  /**
+   * implicit convert to unsigned long long
+   */
+  operator unsigned long long()   const { return atoll(data); }
+  /**
+   * implicit convert to float
+   */
+  operator float()                const { return atof(data); }
+  /**
+   * implicit convert to double
+   */
+  operator double()               const { return atof(data); }
+  /**
+   * implicit convert to c string, if this value is end with '\0'
+   * or you must use GetRawData(). @see GetRawData
+   */
+  operator char*()                const { return const_cast<char*>(data); }
+
+  /**
+   * get the raw data of value, specifically the c string not end with '\0' or
+   * blob type.
+   * @param out dest location
+   * @param l dest buffer size
+   */
+  void GetRawData(void *out, int l)
+  {
+    memcpy(out, data, len < l ? len : l);
+  }
+
+  /**
+   * raw data, cannot hold it after ResultSet is disposed!!!
+   */
+  const char *data;
+  const int  len;
+};
+
+/**
+ * sql param type, prevent sql injection
+ */
+struct SqlParamType
+{
+  int     param_type;
+  std::string data;
+  void* ptr() const { return static_cast<void*>(const_cast<char*>(data.c_str())); }
+  size_t size() const { return data.size(); }
+};
+
+/**
+ * the command of a complex sql, which maybe have dangerous strings.
+ * you can use it like this:
+ *
+ * MySqlCommand command("insert `tb`(`name`, `other`) values (?,?); ");
+ * command.SetParam(0, "egmkang", strlen("egmkang"));
+ * command.SetParam(1, "other", strlen("other"));
+ */
+class SqlCommand : NonCopyable
+{
+ public:
+  /**
+   * @param sql which sql will be executed.
+   */
+  SqlCommand(const char *sql) : sql_(sql) {}
+  virtual ~SqlCommand(){}
+
+  /**
+   * set the param the sql needed.
+   * Only Support VARCHAR and BLOB
+   * @param index which param will be replaced
+   * @param len param's length
+   */
+  virtual SqlCommand& SetParam(int index, void *data, int len) = 0;
+  virtual SqlCommand& SetParam(int index, const char *data, int len) = 0;
+
+  const std::string& GetSql() const { return sql_; }
+  const std::vector<SqlParamType>& GetParams() const { return params; }
+ protected:
+  std::string sql_;
+  std::vector<SqlParamType> params;
+};
+
+#include <mysql/mysql.h>
+
+/**
+ * MySql's Command impl, @see SqlCommand
+ */
+class MySqlCommand : public SqlCommand
+{
+ public:
+  MySqlCommand(const char *sql) : SqlCommand(sql) {}
+  ~MySqlCommand(){}
+
+  virtual SqlCommand& SetParam(int index, void *data, int len);
+  virtual SqlCommand& SetParam(int index, const char *data, int len);
+ private:
+  void SetParam(int index, int type, const void *data, int len);
+};
+
+/**
+ * MySql's DBConnection impl, @see DBConnection
+ */
+class MySqlConnection : public DBConnection
+{
+ public:
+  MySqlConnection();
+  ~MySqlConnection();
+
+  virtual int Connect(const char* host, unsigned short port,const char* user, const char* passwd, const char* db_name);
+  virtual int ExecSql(const char *sql);
+  virtual int ExecSql(const SqlCommand &command);
+  virtual unsigned long long GetNewID();
+
+  virtual ResultSet* ExecSelect(const char *sql);
+  virtual int EscapeString(char *to,const char *from, unsigned long length);
+ private:
+  MYSQL   conn_;
+};
+
+/**
+ * MySql's ResultSet impl, @see ResultSet
+ */
+class MySqlResultSet : public ResultSet
+{
+ public:
+  MySqlResultSet(MYSQL_RES *res);
+  ~MySqlResultSet();
+
+  virtual int FieldCount() const;
+  virtual int RowCount() const;
+  virtual void Next() const;
+  virtual bool IsValid() const;
+
+  virtual SqlResultVarType operator[] (int field) const;
+  virtual SqlResultVarType at(int field) const;
+ private:
+  std::shared_ptr<MYSQL_RES> result;
+  mutable MYSQL_ROW   row;
+  mutable unsigned long *field_len;
+  int                 total_fields;
+  int                 total_rows;
+  mutable int         current_row;
+};
+
+}
+#endif
